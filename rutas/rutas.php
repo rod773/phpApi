eb901f9576bb19615f9565d3f17256535553bc2b
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-18 13:12:17 +0200

    array de rutas

--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,7 +1,17 @@
 <?php
 
+$arrayRutas=explode("/",$_SERVER['REQUEST_URI']);
+
+
+
+echo "<pre>";
+print_r($arrayRutas);
+echo "<pre>";
+
+echo $_SERVER['REQUEST_URI'];
+
 $json = array(
-    "detalle no encontrado"
+    "detalle"=>"no encontrado"
 );
 
 echo json_encode($json,true);

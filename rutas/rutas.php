829fba6422d81566f7af309ad795b4ec50eb7ca3
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-18 14:14:59 +0200

    ruta cursos

--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,18 +1,33 @@
 <?php
 
-$arrayRutas=explode("/",$_SERVER['REQUEST_URI']);
-
-
+$arrayRutas = explode("/", $_SERVER['REQUEST_URI']);
 
 echo "<pre>";
 print_r($arrayRutas);
 echo "<pre>";
 
-echo $_SERVER['REQUEST_URI'];
+if (count(array_filter($arrayRutas)) == 1) {
 
-$json = array(
-    "detalle"=>"no encontrado"
-);
+    $json = array(
+        "detalle" => "no encontrado"
+    );
 
-echo json_encode($json,true);
+    echo json_encode($json, true);
+
+    return;
+} else {
+    if (count(array_filter($arrayRutas)) == 2) {
+        if (array_filter($arrayRutas)[2] == "cursos"
+        ) {
+
+            $json = array(
+                "detalle" => "estas en la vista cursos"
+            );
+
+            echo json_encode($json, true);
+
+            return;
+        }
+    }
+}
 ?>
